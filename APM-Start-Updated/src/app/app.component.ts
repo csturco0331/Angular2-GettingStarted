3b8a89b15198544f2c51a819227b0d2d5496e8e1
Cory Turco <csturco0331@yahoo.com>
2017-07-10 10:07:28 -0400

    added product-list component ts/html

--- APM-Start-Updated/src/app/app.component.ts
+++ APM-Start-Updated/src/app/app.component.ts
@@ -3,7 +3,12 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'pm-app',
     template: `
-        <h1>Angular2: Getting Started</h1>
+    <div>
+        <h1>{{pageTitle}}</h1>
+        <product-list></product-list>
+    </div>
     `
 })
-export class AppComponent { }
+export class AppComponent {
+    pageTitle: string = "Acme Product Management";
+}
